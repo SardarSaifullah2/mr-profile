c35ee6c55fc854a587f733a4107a9ee5b876e77d
saifullah <saifisardar5656@gmail.com>
2024-01-10 08:01:02 -0800

    nitial commit: Uploaded the entire site.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+            hostname : "*.googleusercontent.com"
+        },
+        {
+            hostname : "*.githubusercontent.com"
+        },
+        {
+            hostname : "*.amazonaws.com"
+        }
+    ]
+    }
+}
 
 module.exports = nextConfig
